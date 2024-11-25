691f8ab14e7b910cc31a87a43daa3b0f70915d34
reneelane0 <w0494485@campus.nscc.ca>
2024-11-25 00:45:55 -0400

    included url encoded middleware

--- cineposters.js
+++ cineposters.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path'); // compatibility with file paths ****
+const path = require('path'); // handling file paths ****
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // parse url encoded data ****
 // allows static image files ****
 app.use('/images', express.static(path.join(__dirname, 'public/images'))); 
 
